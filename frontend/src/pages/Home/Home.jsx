44771a37244469dd3b604c1ae376d732280e4270
JuSp12 <justynaspital@gmail.com>
2024-08-04 11:18:13 +0100

    add, remove and show list backend completed

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
 
@@ -13,8 +12,7 @@ const Home = () => {
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
-      <FoodDisplay category={category}/>
-      <AppDownload/>
+    <FoodDisplay category={category}/>
     </div>
   )
 }
