58f1fe9b22e9ad03db0e5a87ac1642346ccd0f55
JuSp12 <justynaspital@gmail.com>
2024-08-10 17:08:04 +0100

    log and logout completed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
@@ -8,7 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = "http://localhost:4000"
   const [token, setToken] = useState("")
-
+  const [food_list, setFoodList] = useState([]);
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -36,6 +35,12 @@ const StoreContextProvider = (props) => {
     }
     return totalAmount;
   }
+
+  useEffect (()=> {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));      
+    }
+  }, [])
  
   const contextValue = {
     food_list,
@@ -45,6 +50,7 @@ const StoreContextProvider = (props) => {
     removeFromCart,
     getTotalCartAmount,
     url, 
+    token,
     setToken
   };
 
