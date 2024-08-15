6817bc77de2d6ee2f0083a505eb6c8299c0623cc
JuSp12 <justynaspital@gmail.com>
2024-08-15 19:58:32 +0100

    payment fixed, now it's displayed in mongoDB

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import './Verify.css'
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const Verify = () => {
 
